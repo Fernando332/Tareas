a334edd9dc9ca83573b1e6b2727bbef92684396d
Fernando332 <160090551+Fernando332@users.noreply.github.com>
2024-03-07 11:03:25 -0700

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,9 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "destinatario@example.com";
+    $subject = "Tarea pendiente";
     $task = $_POST['task'];
     $dueDate = $_POST['dueDate'];
+    $message = "Tarea: $task\nFecha de vencimiento: $dueDate";
 
-    // Aquí puedes enviar un correo electrónico utilizando la función mail()
-    // Asegúrate de configurar correctamente tu servidor para enviar correos electrónicos
+    $headers = "From: remitente@example.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Correo electrónico enviado correctamente.";
+    } else {
+        echo "Error al enviar el correo electrónico. Por favor, intenta nuevamente más tarde.";
+    }
+} else {
+    echo "Acceso denegado.";
 }
 ?>
